c13edb352a8e920d592d1da3490026f6312a6bf0
juan <juangui097@gmail.com>
2023-07-25 03:08:33 -0500

    Added JavaScript
    -Created js folder.
    -Added JavaScript file for functionality.
    -Created Global Elements in JS.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* _________________________________________________
+// -------------  Password Generator  --------------
+// ------------------------------------------------- */
+/* --------  GLOBAL  -------- */
+const lengthSlider = document.querySelector(".pass-range");
+const lengthSliderSpan = document.querySelector(".pass-length_details span");
+const options = document.querySelectorAll(".option input");
+const passwordInput = document.querySelector("#password-input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector("button");
+const copyIcon = document.querySelector(".material-symbols-rounded");
